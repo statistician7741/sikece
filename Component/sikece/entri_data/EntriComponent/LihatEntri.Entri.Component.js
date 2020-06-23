715b16ec91d75fc91319fc93c654cdd33384581e
statistician7741 <shamad2402@gmail.com>
2020-06-24 05:24:36 +0800

    lihat entri tabel

--- Component/sikece/entri_data/EntriComponent/LihatEntri.Entri.Component.js
+++ Component/sikece/entri_data/EntriComponent/LihatEntri.Entri.Component.js
@@ -1,15 +1,110 @@
-import { Row, Col, Select } from 'antd';
+import { Row, Col, Select, Input, Button, Table, Space, Divider, Popconfirm } from 'antd';
 const { Option } = Select
-import { PlusOutlined } from '@ant-design/icons'
-import BasicForm from './Lainnya/BasicForm.Tabel.Component'
-import { Fragment } from 'react';
+import { SearchOutlined } from '@ant-design/icons'
+import Highlighter from 'react-highlight-words';
 import InputForm from '../../general/InputForm.Component'
 
 export default class LihatTabel_Tabel extends React.Component {
+    state = {
+        searchText: '',
+        searchedColumn: ''
+    }
+    getColumnSearchProps = dataIndex => ({
+        filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+            <div style={{ padding: 8 }}>
+                <Input
+                    ref={node => {
+                        this.searchInput = node;
+                    }}
+                    placeholder={`Cari...`}
+                    value={selectedKeys[0]}
+                    onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+                    onPressEnter={() => this.handleSearch(selectedKeys, confirm, dataIndex)}
+                    style={{ width: 188, marginBottom: 8, display: 'block' }}
+                />
+                <Space>
+                    <Button
+                        type="primary"
+                        onClick={() => this.handleSearch(selectedKeys, confirm, dataIndex)}
+                        icon={<SearchOutlined />}
+                        size="small"
+                        style={{ width: 90 }}
+                    >
+                        Cari
+              </Button>
+                    <Button onClick={() => this.handleReset(clearFilters)} size="small" style={{ width: 90 }}>
+                        Reset
+              </Button>
+                </Space>
+            </div>
+        ),
+        filterIcon: filtered => <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />,
+        onFilter: (value, record) =>
+            record[dataIndex] ? record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()) : false,
+        onFilterDropdownVisibleChange: visible => {
+            if (visible) {
+                setTimeout(() => this.searchInput.select());
+            }
+        },
+        render: text =>
+            this.state.searchedColumn === dataIndex ? (
+                <Highlighter
+                    highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+                    searchWords={[this.state.searchText]}
+                    autoEscape
+                    textToHighlight={text ? text.toString() : text}
+                />
+            ) : (
+                    text
+                ),
+    });
+
+    handleSearch = (selectedKeys, confirm, dataIndex) => {
+        confirm();
+        this.setState({
+            searchText: selectedKeys[0],
+            searchedColumn: dataIndex,
+        });
+    };
+
+    handleReset = clearFilters => {
+        clearFilters();
+        this.setState({ searchText: '' });
+    };
     render() {
         const { all_table, all_bab, all_kab, all_kec, bab, selectedYear, years, kab, kec } = this.props
-        const { onClickEdit, onChangeDropdown } = this.props
+        const { onClickEdit, onChangeDropdown, getDynamicTable } = this.props
         console.log(this.props);
+        const tableColumns = [{
+            title: 'No.',
+            dataIndex: 'nomor_tabel',
+            key: '_id',
+            width: 45,
+        }, {
+            title: 'Judul',
+            dataIndex: 'judul',
+            key: '_id',
+            width: 400,
+            showSorterTooltip: false,
+            sorter: (a, b) => a.judul - b.judul,
+            ...this.getColumnSearchProps('judul')
+        }, {
+            title: 'Keterangan',
+            dataIndex: 'ket',
+            ...this.getColumnSearchProps('ket')
+        }, {
+            title: 'pilihan',
+            dataIndex: 'pilihan',
+            fixed: 'right',
+            width: 140,
+            render: (text, record) => <span>
+                <a onClick={() => onClickEdit(`Entri ${record.judul}`, record)}>Edit</a>
+                <Divider type="vertical" />
+                <Popconfirm title={`Hapus entrian di tabel ini? Ini tidak akan menghapus tabel`} onConfirm={() => this.props.dispatch(deleteTablebyId(this.props.socket, record._id, this.props))}>
+                    <a>Hapus</a>
+                </Popconfirm>
+            </span>
+        }]
         return (
             <Col xs={24}>
                 <Row gutter={[64, 0]}>
@@ -30,7 +125,7 @@ export default class LihatTabel_Tabel extends React.Component {
                             </Select>
                         </InputForm>
                         <InputForm xs={19} name='Kabupaten' isWajib={false} left>
-                            <Select defaultValue={kab} value={kab} style={{ width: 200 }} onChange={kab => onChangeDropdown({kab})}>
+                            <Select defaultValue={kab} value={kab} style={{ width: 200 }} onChange={kab => onChangeDropdown({ kab })}>
                                 {all_kab.map(k => <Option value={k._id} key={k._id}>[{k._id}] {k.name}</Option>)}
                             </Select>
                         </InputForm>
@@ -45,6 +140,21 @@ export default class LihatTabel_Tabel extends React.Component {
                                 {all_bab.filter(bab => selectedYear == bab.tahun_buku).map(b => <Option value={b._id} key={b._id}>Bab {b.nomor}. {b.name}</Option>)}
                             </Select>
                         </InputForm>
+                    </Col>
+                </Row>
+                <Row gutter={[64, 0]}>
+                    <Col xs={24}>
+                        <Table
+                            scroll={{ x: 1200 }}
+                            size="small"
+                            columns={tableColumns}
+                            dataSource={all_table.filter(t => ( selectedYear==t.bab.substring(0, 4) && (bab === 'all_bab' || bab === t.bab)) )}
+                            rowKey="_id"
+                            expandable={{
+                                expandedRowRender: ({ baris, kolom, nomor_tabel, judul, sumber, catatan }) => getDynamicTable(baris, kolom, nomor_tabel, judul, sumber, catatan),
+                            }}
+                            pagination={{ defaultPageSize: 15, showSizeChanger: true, position: 'top', pageSizeOptions: ['15', '30', '50', '100', '200', '500'], showTotal: (total, range) => `${range[0]}-${range[1]} dari ${total} tabel` }}
+                        />
                     </Col>
                 </Row>
             </Col>
--- Component/sikece/entri_data/index.Component.js
+++ Component/sikece/entri_data/index.Component.js
@@ -1,6 +1,6 @@
-import { Row, PageHeader } from "antd"
+import { Row, PageHeader, Col, Table } from "antd"
 import dynamic from 'next/dynamic';
-import { getKec, getTable, getKab, getBab } from "../../../redux/actions/master.action"
+import { getKec, getTable, getKab, getBab, getVariable } from "../../../redux/actions/master.action"
 
 const LihatEntri = dynamic(() => import("./EntriComponent/LihatEntri.Entri.Component"));
 const EditorEntri = dynamic(() => import("./EntriComponent/EditorEntri.Entri.Component"));
@@ -30,6 +30,118 @@ export default class IndexEntri extends React.Component {
             }
         })
     }
+    getBarisDataSource = (baris) => {
+        let judul_baris = [];
+        baris.forEach((_id, i) => {
+            if (this.props.all_variable_obj[_id].name.match(/^Desa\s?\/?\s?(Kelurahan)?\s?$/)) {
+                if (this.props.all_deskel.length) {
+                    let id_kec = this.props.all_deskel[0].kec
+                    this.props.all_deskel.filter(d => (d.kec === id_kec)).forEach((d, i) => {
+                        judul_baris.push(`${d.kode} ${d.name}`)
+                    })
+                } else {
+                    judul_baris.push(this.props.all_variable_obj[_id].name)
+                }
+            } else if (this.props.all_variable_obj[_id].name.match(/^Jumlah|Total\s?$/) && i === baris.length - 1) {
+                //not pushing
+            } else {
+                judul_baris.push(this.props.all_variable_obj[_id].name)
+            }
+        })
+        return judul_baris.map((d, i) => ({ '_id': i, 'baris_var': d, 'data': "data" }))
+    }
+    getDynamicTable = (baris, kolom, nomor_tabel, judul, sumber, catatan) => {
+        const { all_variable_obj, all_kec } = this.props;
+        return <Row>
+            <Col xs={24}>
+                <Row justify="center" style={{ textAlign: "center" }}>
+                    <Col xs={24}>
+                        <strong>{`Tabel ${nomor_tabel ? nomor_tabel : '[Nomor Tabel]'}`}</strong>
+                    </Col>
+                </Row>
+                <Row justify="center" style={{ textAlign: "center" }} gutter={[0, 16]}>
+                    <Col xs={24}>
+                        <strong>{judul ? judul.replace('{nama}', all_kec.length ? all_kec[0].name : 'A') : '[Judul Tabel]'}</strong>
+                    </Col>
+                </Row>
+                <Row gutter={[0, 8]}>
+                    <Col xs={24}>
+                        {all_variable_obj !== {} && baris.length && kolom.length ? <Table
+                            size="small"
+                            bordered
+                            columns={(() => {
+                                const parents = {}
+                                const cols = []
+                                let judul_kelompok_baris = ''
+                                baris.forEach((_idBaris, i) => {
+                                    if (all_variable_obj[_idBaris].kelompok && judul_kelompok_baris === '') judul_kelompok_baris = all_variable_obj[_idBaris].kelompok
+                                })
+                                cols.push({
+                                    title: judul_kelompok_baris === '' ? all_variable_obj[baris[0]].name : judul_kelompok_baris,
+                                    dataIndex: "baris_var"
+                                })
+                                kolom.forEach((_idKolom, i) => {
+                                    if (all_variable_obj[_idKolom].kelompok !== "-" && all_variable_obj[_idKolom].kelompok !== "" && all_variable_obj[_idKolom].kelompok) {
+                                        if (!parents[all_variable_obj[_idKolom].kelompok]) {
+                                            parents[all_variable_obj[_idKolom].kelompok] = {};
+                                            parents[all_variable_obj[_idKolom].kelompok].indexAnggota = [];
+                                        }
+                                        parents[all_variable_obj[_idKolom].kelompok].indexAnggota.push(_idKolom)
+                                    } else {
+                                        if (!parents[`no_parents_${i}`]) {
+                                            parents[`no_parents_${i}`] = {};
+                                            parents[`no_parents_${i}`].indexAnggota = [];
+                                        }
+                                        parents[`no_parents_${i}`].indexAnggota.push(_idKolom)
+                                    }
+                                })
+                                for (var parent in parents) {
+                                    if (parents.hasOwnProperty(parent)) {
+                                        if (parent.includes('no_parents')) {
+                                            cols.push({
+                                                title: all_variable_obj[parents[parent].indexAnggota[0]].name,
+                                                dataIndex: "data",
+                                                align: 'right'
+                                            })
+                                        } else {
+                                            const calon_col = {}
+                                            calon_col.title = parent
+                                            calon_col.children = parents[parent].indexAnggota.map(indexVar => ({
+                                                title: all_variable_obj[indexVar].name,
+                                                dataIndex: "data",
+                                                align: 'right'
+                                            }));
+                                            cols.push(calon_col)
+                                        }
+                                    }
+                                }
+                                return cols
+                            })()}
+                            dataSource={this.getBarisDataSource(baris)}
+                            pagination={false}
+                            rowKey="_id"
+                            summary={() => (this.props.all_variable_obj[baris[baris.length - 1]].name.match(/^Jumlah|Total\s?$/) ?
+                                <Table.Summary.Row style={{ background: '#fafafa', textAlign: 'right' }}>
+                                    <Table.Summary.Cell index={0}><strong style={{ float: 'left' }}>{this.props.all_variable_obj[baris[baris.length - 1]].name}</strong></Table.Summary.Cell>
+                                    {kolom.map((k, i) => <Table.Summary.Cell index={i + 1}><strong>data</strong></Table.Summary.Cell>)}
+                                </Table.Summary.Row> : undefined)
+                            }
+                        /> : 'Belum ada Variabel'}
+                    </Col>
+                </Row>
+                <Row gutter={[0, catatan ? 0 : 16]}>
+                    <Col xs={24}>
+                        Sumber: {sumber ? sumber.replace('{nama}', all_kec.length ? all_kec[0].name : 'A') : ''}
+                    </Col>
+                </Row>
+                {catatan ? <Row gutter={[0, 16]}>
+                    <Col xs={24}>
+                        Catatan: {catatan.replace('{nama}', all_kec.length ? all_kec[0].name : 'A')}
+                    </Col>
+                </Row> : null}
+            </Col>
+        </Row>
+    }
     onChangeDropdown = (data) => this.setState(data)
     componentDidMount() {
         if (this.props.socket) {
@@ -38,6 +150,7 @@ export default class IndexEntri extends React.Component {
             !this.props.all_kab.length && this.props.dispatch(getKab(this.props.socket))
             !this.props.all_kec.length && this.props.dispatch(getKec(this.props.socket))
             !this.props.all_bab.length && this.props.dispatch(getBab(this.props.socket))
+            !this.props.all_variable.length && this.props.dispatch(getVariable(this.props.socket))
         }
     }
     componentDidUpdate(prevProps, prevState) {
@@ -47,15 +160,16 @@ export default class IndexEntri extends React.Component {
             this.props.dispatch(getKab(this.props.socket))
             this.props.dispatch(getKec(this.props.socket))
             this.props.dispatch(getBab(this.props.socket))
+            this.props.dispatch(getVariable(this.props.socket))
         }
         if (this.props.all_kab !== prevProps.all_kab && this.props.all_kab.length) {
             this.setState({ kab: this.props.all_kab[0]._id })
         }
         if (this.props.all_kec !== prevProps.all_kec && this.props.all_kec.length && this.state.kab) {
-            this.props.all_kec.filter(kec => this.state.kab === kec.kab).length&&this.setState({ kec: this.props.all_kec.filter(kec => this.state.kab === kec.kab)[0]._id })
+            this.props.all_kec.filter(kec => this.state.kab === kec.kab).length && this.setState({ kec: this.props.all_kec.filter(kec => this.state.kab === kec.kab)[0]._id })
         }
         if (this.state.kab !== prevState.kab && this.state.kab) {
-            this.props.all_kec.filter(kec => this.state.kab === kec.kab).length&&this.setState({ kec: this.props.all_kec.filter(kec => this.state.kab === kec.kab)[0]._id })
+            this.props.all_kec.filter(kec => this.state.kab === kec.kab).length && this.setState({ kec: this.props.all_kec.filter(kec => this.state.kab === kec.kab)[0]._id })
         }
     }
 
@@ -70,7 +184,7 @@ export default class IndexEntri extends React.Component {
             >
                 {activePage === 'list' ?
                     <Row gutter={[16, 0]}>
-                        <LihatEntri {...this.props} years={years} kab={kab} kec={kec} bab={bab} selectedYear={selectedYear} onChangeDropdown={this.onChangeDropdown} onClickEntri={this.onClickEntri} />
+                        <LihatEntri {...this.props} years={years} kab={kab} kec={kec} bab={bab} selectedYear={selectedYear} onChangeDropdown={this.onChangeDropdown} onClickEntri={this.onClickEntri} getDynamicTable={this.getDynamicTable} />
                     </Row> :
                     <Row gutter={[16, 0]}>
                         <EditorEntri {...this.props} xs={24} kecData={kecData} loadDataKec={this.loadDataKec} indikators={indikators} loadDataIndikators={this.loadDataIndikators} cascaderFilter={this.cascaderFilter} />
