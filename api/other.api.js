6a68e500353e1892d9cce234b55fda04b8d19b60
Muh. Shamad SST <shamad2402@gmail.com>
2021-12-17 16:44:48 +0800

    copy year tabel

--- api/other.api.js
+++ api/other.api.js
@@ -10,21 +10,28 @@ const User = require("../models/User.model")
 
 const static_path = __dirname + "/../public/static/"
 const template_file = `Surat_Pernyataan_Pengisian_Data.docx`;
+
 function download(req, res) {
-  User.findOne({ '_id': req.cookies.user_id }, (err, user) => {
+  User.findOne({
+    '_id': req.cookies.user_id
+  }, (err, user) => {
     if (user) {
-      const outputDocx = static_path + `${user.tahun_buku}_Surat_Pernyataan_Pengisian_Data.docx`;//
+      const outputDocx = static_path + `${user.tahun_buku}_Surat_Pernyataan_Pengisian_Data.docx`; //
       if (fs.existsSync(outputDocx)) {
         res.download(outputDocx);
       } else {
-        const template = fs.readFileSync(static_path + template_file, "binary");//
+        const template = fs.readFileSync(static_path + template_file, "binary"); //
         const zip = new JSZip(template);
         const docx = new Docxtemplater().loadZip(zip);
-        docx.setData({ tahun_buku: user.tahun_buku });
+        docx.setData({
+          tahun_buku: user.tahun_buku
+        });
         //render
         docx.render();
         const buf = docx.getZip()
-          .generate({ type: "nodebuffer" });
+          .generate({
+            type: "nodebuffer"
+          });
         fs.writeFileSync(outputDocx, buf);
         res.download(outputDocx);
       }
@@ -33,6 +40,7 @@ function download(req, res) {
     }
   })
 }
+
 function downloadArsip(req, res) {
   if (req.params.file) {
     const outputDocx = static_path + `arsip/${req.params.file}`;
@@ -46,7 +54,83 @@ function downloadArsip(req, res) {
   }
 }
 
+function copyTable(fromYear, toYear, res) {
+  const Tabel = require('../models/Table.model')
+  const Bab = require('../models/Bab.model')
+
+  Bab.find({
+    tahun_buku: fromYear
+  }, (e, r) => {
+    console.log(r.length)
+    r.forEach(bab => {
+      const {
+        _id,
+        tahun_buku,
+        nomor,
+        name,
+        ket
+      } = bab
+      Bab.create({
+        _id: `${toYear}_${nomor}`,
+        tahun_buku: toYear,
+        nomor,
+        name,
+        ket
+      }, (e, b) => {
+        //BAB BARU CREATED
+        Tabel.find({
+          bab: bab._id
+        }, (e, r) => {
+          console.log('Tabel: ', r.length)
+          r.forEach(tabel => {
+            const {
+              bab,
+              nomor_tabel,
+              judul,
+              baris,
+              kolom,
+              sumber,
+              catatan,
+              ket
+            } = tabel
+            Tabel.create({
+              bab: `${toYear}_${bab.match(/\d+$/)[0]}`,
+              nomor_tabel,
+              judul: judul.replace(fromYear - 1, fromYear),
+              baris,
+              kolom,
+              sumber,
+              catatan,
+              ket
+            }, (e, t) => {
+              console.log('Tabel baru: ', t.judul)
+            })
+          })
+        })
+      })
+    })
+  })
+
+  setTimeout(() => {
+    res.redirect('/')
+  }, 3000)
+}
+
 router.get("/template", download)
 router.get("/arsip/:file", downloadArsip)
+
+router.get("/copy/:fromyear/:toyear", (req, res) => {
+  const fromYear = parseInt(req.params.fromyear);
+  const toYear = parseInt(req.params.toyear);
+  if (Number.isInteger(fromYear) && Number.isInteger(toYear)) {
+    if (
+      (fromYear >= 2021 && toYear >= 2022) &&
+      (toYear > fromYear)
+    ) copyTable(fromYear, toYear, res)
+    else res.status(400).send('Cek parameter');
+  } else {
+    res.status(400).send('Cek parameter');
+  }
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -22,7 +22,9 @@ const MongoStore = require('connect-mongo')(session);
 let runServer = () => {
   const port = 80
   const dev = process.env.NODE_ENV !== 'production'
-  const app = next({ dev })
+  const app = next({
+    dev
+  })
   const handle = app.getRequestHandler()
 
   app.prepare()
@@ -36,11 +38,15 @@ let runServer = () => {
         resave: true,
         saveUninitialized: true,
         secret: "ID==&&%^&A&SHBJSAsjhbJGhUGkbKiUvii^%^#$%^&98G8UIugg==",
-        store: new MongoStore({ mongooseConnection: mongoose.connection })
+        store: new MongoStore({
+          mongooseConnection: mongoose.connection
+        })
       })
       server.use(sessionWithMongo);
       server.use(cookieParser("ID==&&%^&A&SHBJSAsjhbJGhUGkbKiUvii^%^#$%^&98G8UIugg=="));
-      server.use(bodyParser.urlencoded({ extended: true }));
+      server.use(bodyParser.urlencoded({
+        extended: true
+      }));
       server.use(bodyParser.json())
 
       server.use('/sikece', require("./api/login.api"));
@@ -76,7 +82,9 @@ let runServer = () => {
         if (err) throw err
         console.log(`> Ready on https://localhost:${port}`)
         const User = require('./models/User.model')
-        User.findOne({ "username": 'ipds' }, (err, result) => {
+        User.findOne({
+          "username": 'ipds'
+        }, (err, result) => {
           const data = {
             'username': 'ipds',
             'password': '@ipds',
@@ -85,49 +93,9 @@ let runServer = () => {
             'ket': 'Login default'
           }
           if (!result) {
-            User.create(data, (err, result) => { })
+            User.create(data, (err, result) => {})
           }
         })
-
-
-        //copy 2021
-        // const Tabel = require('./models/Table.model')
-        // const Bab = require('./models/Bab.model')
-
-        // Tabel.find({}, (e,r)=>{
-        //   console.log(r.length)
-        //   r.forEach(tabel=>{
-        //     const { bab, nomor_tabel, judul, baris, kolom, sumber, catatan, ket } = tabel
-        //     Tabel.create({
-        //       bab: `2021_${bab.match(/\d+$/)[0]}`,
-        //       nomor_tabel,
-        //       judul: judul.replace('2019', '2020'),
-        //       baris,
-        //       kolom,
-        //       sumber,
-        //       catatan,
-        //       ket
-        //     }, (e,t)=>{
-        //       console.log(t._id)
-        //     })
-        //   })
-        // })
-
-        // Bab.find({}, (e,r)=>{
-        //   console.log(r.length)
-        //   r.forEach(bab=>{
-        //     const { _id, tahun_buku, nomor, name, ket } = bab
-        //     Bab.create({
-        //       _id: `2021_${nomor}`,
-        //       tahun_buku: 2021,
-        //       nomor,
-        //       name,
-        //       ket
-        //     }, (e,b)=>{
-        //       console.log(b._id)
-        //     })
-        //   })
-        // })
 
       })
 
@@ -138,8 +106,11 @@ let runServer = () => {
         onlineUser.forEach(u => {
           if (u.name === name) isExist = true
         })
-        if (!isExist){
-          onlineUser = [...onlineUser, {name, profil}]
+        if (!isExist) {
+          onlineUser = [...onlineUser, {
+            name,
+            profil
+          }]
         }
       }
       const removeOnlineUser = (name) => {
@@ -147,7 +118,11 @@ let runServer = () => {
       }
       io.use(sharedsession(sessionWithMongo, cookieParser("ID==&&%^&A&SHBJSAsjhbJGhUGkbKiUvii^%^#$%^&98G8UIugg==")));
       io.on('connection', function (client) {
-        const { name, jenis_pengguna, profil } = client.handshake.cookies
+        const {
+          name,
+          jenis_pengguna,
+          profil
+        } = client.handshake.cookies
         require('./api/master_table.api')(client)
         require('./api/master_user.api')(client)
         require('./api/general.api')(client)
@@ -165,8 +140,14 @@ let runServer = () => {
         client.on('getOnlineUser', () => {
           client.emit('refreshOnlineUser', onlineUser);
         })
-        client.on('sendChat', ({msg, from}) => {
-          client.broadcast.emit('newChat', {msg, from});
+        client.on('sendChat', ({
+          msg,
+          from
+        }) => {
+          client.broadcast.emit('newChat', {
+            msg,
+            from
+          });
         })
         if (jenis_pengguna) {
           pushOnlineUser(name, profil)
@@ -178,10 +159,15 @@ let runServer = () => {
 }
 
 //modul mongodb utk koneksi mongo db database
-const { exec } = require('child_process');
+const {
+  exec
+} = require('child_process');
 
 let start = () => {
-  mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+  mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }, (err) => {
     if (err) {
       exec(`powershell -Command "Start-Process cmd -Verb RunAs -ArgumentList '/c net start MongoDB'"`, (err, stdout, stderr) => {
         console.log('Trying to start MongoDB service...');
